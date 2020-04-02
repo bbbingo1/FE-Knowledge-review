12508e13846f31b2a7a19e6335de49719c389f6b
bbbingo1 <846790344@qq.com>
2020-04-02 17:59:12 +0800

    算法刷刷刷

---
+++ 算法/双指针/环形链表.js
@@ -0,0 +1,34 @@
+/**
+ * 给定一个链表，判断链表中是否有环。
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+ 来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/linked-list-cycle
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+    if (!head) return false;
+    let fast = head;
+    let slow = head;
+
+    while (fast && slow && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast == slow) {
+            return true;
+        }
+    }
+    return false;
+};
